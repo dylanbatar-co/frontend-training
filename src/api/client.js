545c09bbfd936c2788c3d2adadfc1168b2874f9e
dylanbatar-co <dbatista@condorlabs.io>
2020-12-24 11:35:28 -0500

    upload docs

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 const KEY = '2e451bcc8dfbac73fe2a37ebb97cc9c5';
-const URL = `http://api.mediastack.com/v1/news?access_key=${KEY}&limit=20?languages=en`;
+const URL = `https://api.mediastack.com/v1/news?access_key=${KEY}&limit=20?languages=en`;
 
 const getNews = async () => {
   const response = await fetch(URL);
