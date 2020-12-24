5f86e4afb18ab4d4eb424d03362a9b14781df600
dylanbatar-co <dbatista@condorlabs.io>
2020-12-24 10:24:47 -0500

    deploy netlify

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 const KEY = 'ca019f5e52e841109f87d8766bc040bc';
-const URL = `http://newsapi.org/v2/top-headlines?apiKey=${KEY}&country=us&pageSize=20`;
+const URL = `https://newsapi.org/v2/top-headlines?apiKey=${KEY}&country=us&pageSize=20`;
 
 const getNews = async () => {
   const response = await fetch(URL);
